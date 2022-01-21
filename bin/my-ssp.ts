97e304c078f5c04c8f883ba128057dc33db410f7
EC2 Default User <ec2-user@ip-172-31-53-2.us-west-2.compute.internal>
2022-01-21 20:31:46 +0000

    adding teams part 2

--- bin/my-ssp.ts
+++ bin/my-ssp.ts
@@ -1,5 +1,6 @@
 import { App } from '@aws-cdk/core'
 import * as ssp from '@aws-quickstart/ssp-amazon-eks';
+import { TeamPlatform, TeamApplication } from '../teams';
 
 const app = new App();
 const account = process.env.CDK_DEFAULT_ACCOUNT!;
@@ -10,7 +11,7 @@ const blueprint = ssp.EksBlueprint.builder()
   .account(account)
   .region(region)
   .addOns()
-  .teams();
+  .teams(new TeamPlatform(account), new TeamApplication('burnham'));
   
 // Build Codepipeline
 ssp.CodePipelineStack.builder()
