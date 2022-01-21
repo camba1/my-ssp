eb0c2fef46631e3e3e615f85345514b350da529b
EC2 Default User <ec2-user@ip-172-31-53-2.us-west-2.compute.internal>
2022-01-21 19:15:36 +0000

    Setting up SSP deployment pipeline

---
+++ bin/my-ssp.ts
@@ -0,0 +1,29 @@
+import { App } from '@aws-cdk/core'
+import * as ssp from '@aws-quickstart/ssp-amazon-eks';
+
+const app = new App();
+const account = process.env.CDK_DEFAULT_ACCOUNT!;
+const region = process.env.CDK_DEFAULT_REGION;
+const env = { account, region };
+
+const blueprint = ssp.EksBlueprint.builder()
+  .account(account)
+  .region(region)
+  .addOns()
+  .teams();
+  
+// Build Codepipeline
+ssp.CodePipelineStack.builder()
+  .name("ssp-eks-workshop-pipeline")
+  .owner("camba1")
+  .repository({
+      repoUrl: 'my-ssp',
+      credentialsSecretName: 'github-token',
+      targetRevision: 'main'
+  })
+    // Add stages
+  .stage({
+    id: 'prod',
+    stackBuilder: blueprint.clone('us-west-2')
+  })
+  .build(app, 'pipeline-stack', {env});
